cc65240c0a0e25649661bb043dc97a946fce3436
Pratik <pratikahir787@gmail.com>
2023-12-27 18:41:20 +0530

    27/12

---
+++ Defi/src/App.jsx
@@ -0,0 +1,23 @@
+import About from "./Components/About"
+import Developers from "./Components/Developers"
+import Footer from "./Components/Footer"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+import Subscribe from "./Components/Subscribe"
+
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Developers />
+      <Subscribe />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ Defi/src/Components/AboutCard.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+
+const AboutCard = (props) => {
+  return (
+    <div className='flex flex-col border text-left rounded-2xl py-12 px-8'>
+    <div>
+      <div className='bg-[#00d8ff] inline-flex p-2 rounded-full'>
+        {props.icon}
+      </div>
+      <h3 className='text-xl font-bold py-4'>{props.heading}</h3>
+      <p>
+        {props.text}
+      </p>
+    </div>
+  </div>
+  )
+}
+
+export default AboutCard
